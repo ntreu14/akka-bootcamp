5b2af52d1a1908fab8e47029f70ef3c84c61a9ff
Nicholas Treu <nicholas.treu@autostoresystem.com>
2022-01-20 15:43:55 -0700

    Complete lesson 1.2 in C#

--- src/Unit-1/CSharp/DoThis/WinTail/ConsoleReaderActor.cs
+++ src/Unit-1/CSharp/DoThis/WinTail/ConsoleReaderActor.cs
@@ -7,8 +7,9 @@ namespace WinTail
 	/// Actor responsible for reading FROM the console. 
 	/// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
 	/// </summary>
-	class ConsoleReaderActor : UntypedActor
+	public class ConsoleReaderActor : UntypedActor
 	{
+		public const string StartCommand = "start";
 		public const string ExitCommand = "exit";
 		private IActorRef _consoleWriterActor;
 
@@ -19,20 +20,54 @@ namespace WinTail
 
 		protected override void OnReceive(object message)
 		{
-			var read = Console.ReadLine();
-			if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+			if (message.Equals(StartCommand))
 			{
-				// shut down the system (acquire handle to system via
-				// this actors context)
-				Context.System.Terminate();
-				return;
+				DoPrintInstructions();
+			}
+			else if (message is InputError error)
+			{
+				_consoleWriterActor.Tell(error);
 			}
 
-			// send input to the console writer to process and print
-			_consoleWriterActor.Tell(read);
+			GetAndValidateInput();
+		}
 
-			// continue reading messages from the console
-			Self.Tell("continue");
+		private void GetAndValidateInput()
+		{
+			var message = Console.ReadLine();
+			if (string.IsNullOrEmpty(message))
+			{
+				// signal that the user needs to supply an input, as previously
+				// received input was blank
+				Self.Tell(new NullInputError("No input received."));
+			}
+			else if (string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+			{
+				// shut down the entire actor system (allows the process to exit)
+				Context.System.Terminate();
+			}
+			else
+			{
+				var valid = IsValid(message);
+				if (valid)
+				{
+					_consoleWriterActor.Tell(new InputSuccess("Thank you! Message was valid."));
+				}
+				else
+				{
+					Self.Tell(new ValidationError("Invalid: Input had off number  of characters"));
+				}
+			}
+		}
+
+		private bool IsValid(string message) =>
+			message.Length % 2 == 0;
+
+		private void DoPrintInstructions()
+		{
+			Console.WriteLine("Write whatever you want into the console!");
+			Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+			Console.WriteLine("Type 'exit' to quit this application at any time.\n");
 		}
 	}
 }
--- src/Unit-1/CSharp/DoThis/WinTail/ConsoleWriterActor.cs
+++ src/Unit-1/CSharp/DoThis/WinTail/ConsoleWriterActor.cs
@@ -7,28 +7,26 @@ namespace WinTail
 	/// Actor responsible for serializing message writes to the console.
 	/// (write one message at a time, champ :)
 	/// </summary>
-	class ConsoleWriterActor : UntypedActor
+	public class ConsoleWriterActor : UntypedActor
 	{
 		protected override void OnReceive(object message)
 		{
-			var msg = message as string;
-
-			// make sure we got a message
-			if (string.IsNullOrEmpty(msg))
+			if (message is InputError error)
 			{
-				Console.ForegroundColor = ConsoleColor.DarkYellow;
-				Console.WriteLine("Please provide an input.\n");
-				return;
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine(error.Reason);
+			}
+			else if (message is InputSuccess msg)
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine(msg.Reason);
+			}
+			else
+			{
+				Console.WriteLine(message);
 			}
 
-			// if message has even # characters, display in red; else, green
-			var even = msg.Length % 2 == 0;
-			var color = even ? ConsoleColor.Red : ConsoleColor.Green;
-			var alert = even ? "Your string had an even # of characters.\n" : "Your string had an odd # of characters.\n";
-			Console.ForegroundColor = color;
-			Console.WriteLine(alert);
 			Console.ResetColor();
-
 		}
 	}
 }
