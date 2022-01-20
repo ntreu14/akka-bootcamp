208f873a107c8c9102362317b0f8b6f4650ef98c
Nicholas Treu <nicholas.treu@autostoresystem.com>
2022-01-20 14:24:46 -0700

    Complete F# lesson 1.1

--- src/Unit-1/FSharp/DoThis/WinTail-FSharp/Program.fs
+++ src/Unit-1/FSharp/DoThis/WinTail-FSharp/Program.fs
@@ -1,1 +1,16 @@
-﻿
+﻿open Akka.FSharp
+open Akka.FSharp.Spawn
+open Akka.Actor
+open WinTail
+
+let myActorSystem = System.create "MyActorSystem" <| Configuration.load ()
+
+let consoleWriterActor = 
+    spawn myActorSystem "consoleWriterActor" <| actorOf Actors.consoleWriterActor
+
+let consoleReaderActor = 
+    Actors.consoleReaderActor consoleWriterActor
+    |> actorOf2
+    |> spawn myActorSystem "conosleWriterActor"
+
+consoleReaderActor <! Actors.Start
